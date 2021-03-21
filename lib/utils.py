f58b7c350b3c9e1095779ffa13ac5e048d54210a
Adam Pacholski <apachols@gmail.com>
2021-03-21 13:50:37 -0700

    Add StepTimer function to utils

--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,14 @@
-
+import time
 
 def test_a_python_import_from_github():
   print('Success!')
 
+
+class StepTimer(object):
+    def __init__(self, description):
+        self.description = description
+    def __enter__(self):
+        self.start = time.time()
+    def __exit__(self, type, value, traceback):
+        self.end = time.time()
+        print(f"{self.description}: {self.end - self.start}")
